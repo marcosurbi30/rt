2d30c21d73e23c7e6539c9422cad56f3b78b0102
marcosurbi30 <marcos_urbi30@hotmail.com>
2025-04-10 15:30:34 +0200

    navbar y modificaciones para conexion con nav y app

---
+++ rt/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import logo from "/assets/logo.png";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+      <div className="container px-4 mx-auto relative text-sm">
+        <div className="flex justify-center-items-center">
+          <div className="flex items-center flex-shrink-0">
+            <img src={logo} alt="logo" />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- rt/src/App.jsx
+++ rt/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import Navbar from "../components/Navbar.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
     </>
   );
 }
