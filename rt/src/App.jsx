f4735860a500e8275ca04062d00eff8e2a39e4b5
marcosurbi30 <marcos_urbi30@hotmail.com>
2025-04-10 20:17:01 +0200

    hero section, con videos por terminar

--- rt/src/App.jsx
+++ rt/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Navbar from "../components/Navbar.jsx";
+import HeroSection from "../components/HeroSection.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,6 +8,9 @@ function App() {
   return (
     <>
       <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+      </div>
     </>
   );
 }
