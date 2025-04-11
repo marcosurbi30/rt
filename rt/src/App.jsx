216883a327855bc07e3656c5112793fe2ea54fc3
marcosurbi30 <marcos_urbi30@hotmail.com>
2025-04-11 11:46:02 +0200

    empezando featuresSection

--- rt/src/App.jsx
+++ rt/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Navbar from "../components/Navbar.jsx";
 import HeroSection from "../components/HeroSection.jsx";
+import FeaturesSection from "../components/FeaturesSection.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
+        <FeaturesSection />
       </div>
     </>
   );
