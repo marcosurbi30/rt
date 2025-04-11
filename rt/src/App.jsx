6e9cc6ada607be362c5189196c66643804a506a7
marcosurbi30 <marcos_urbi30@hotmail.com>
2025-04-11 15:26:23 +0200

    componente Workflow iniciado solo falta la funcion para los arrays

--- rt/src/App.jsx
+++ rt/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Navbar from "../components/Navbar.jsx";
 import HeroSection from "../components/HeroSection.jsx";
 import FeatureSection from "../components/FeatureSection.jsx";
+import Workflow from "../components/Workflow.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +13,7 @@ function App() {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
         <FeatureSection />
+        <Workflow />
       </div>
     </>
   );
